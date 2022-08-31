bad4fea5a4c759144b848442e0328183dc24d364
tagashira <tagashira@zuiki.co.jp>
2022-08-31 15:18:32 +0900

    Update Commit For nuttx source  2022-08-31(15:18)

--- gitall.sh
+++ gitall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-rm -rf .git_credentials
+#rm -rf .git_credentials
 git add .
 git commit -m "Update Commit For nuttx source  `date '+%F(%H:%M)'` "
 git push -f -u origin --all
-
+#rm -rf .git_credentials
